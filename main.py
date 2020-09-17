394d8ca3996ceaa573315ab09278f7224dc9c4de
Mauricio Aspiazo <mau@tauruseer.com>
2020-09-17 13:51:54 -0500

    Multi process version

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from multiprocessing import Process
+import time
 
 def crawl_url(browse_url):
     url = browse_url
+    time.sleep(10)
     page = urlopen(url)
     html_bytes = page.read()
     html = html_bytes.decode("utf-8")
@@ -18,13 +20,31 @@ def clean_html(raw_html):
         names.append(tag)
     return names
 
-def print_data(url):
-    html = crawl_url(url)
-    dt = clean_html(html)
-    print(dt)
+def print_data(url=""):
+    if(url):
+        html = crawl_url(url)
+        dt = clean_html(html)
+        print(dt)
+    else:
+        print("Nothing to show...")
 
 if __name__ == "__main__":
-    url = "https://www.ebay.com/sch/i.html?_nkw=rtx+3080"
-    print_data(url)
+    
+    urls = ["https://www.ebay.com/sch/i.html?_nkw=rtx+3080",
+    "https://www.ebay.com/sch/i.html?_nkw=rtx+2080",
+    "https://www.ebay.com/sch/i.html?_nkw=rtx+2060"]
+
+    procs = []
+    proc = Process(target=print_data)
+    procs.append(proc)
+    proc.start()
+
+    for url in urls:
+        proc = Process(target=print_data, args=(url,))
+        procs.append(proc)
+        proc.start()
+    
+    for process in procs:
+        process.join()
 
 
